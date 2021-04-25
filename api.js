6939e5993d0fcc01b86e2722a0261d1751084baa
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2021-04-25 13:43:53 +0200

    feat: compilation

--- api.js
+++ api.js
@@ -1,23 +1,37 @@
 const express = require('express');
 const fs = require('fs');
 const childProcess = require('child_process');
+const rimraf = require('rimraf');
+const path = require('path');
 
 
 
 const api = express.Router();
 
-api.get("/compiled.so", (req, res) => {
+api.post("/compiled.so", async (req, res) => {
     const b = req.body;
-    if (!b.python | !b.cpp) return res.status(400).end();
+    // console.log(b);
+    if (!b.python || !b.cpp || !b.filename) return res.status(400).end();
+    if (b.filename.includes("\\") || b.filename.includes("/")) return res.status(400).end();
+    await rm("./compilation/")
     if (!fs.existsSync("./compilation/")) fs.mkdirSync("./compilation/");
-    if (fs.existsSync("./compilation/lib.cpp")) fs.unlinkSync("./compilation/lib.cpp");
-    if (fs.existsSync("./compilation/setup.py")) fs.unlinkSync("./compilation/setup.py");
     fs.writeFileSync("./compilation/setup.py", b.python);
-    fs.writeFileSync("./compilation/lib.cpp", b.cpp);
+    fs.writeFileSync("./compilation/" + b.filename, b.cpp);
+    fs.mkdirSync("./compilation/" + b.filename.replace("_module.cpp", ""));
+    console.log(childProcess.execSync(`cd compilation && py ./setup.py build`).toString());
 
-    console.log(childProcess.execSync(`cd compilation && python ./setup.py build`).toString());
+    const f = path.join("./compilation/build/", fs.readdirSync("./compilation/build/").find(v => v.startsWith("lib")));
 
+    const file = fs.readFileSync(path.join(f, fs.readdirSync(f).find(v => v.endsWith(".pyd"))));
 
+    res.status(200).write(file);
+    res.end();
 });
-
+function rm(pth) {
+    return new Promise((resolve, reject) => {
+        rimraf(pth, () => {
+            resolve();
+        });
+    });
+}
 module.exports = api;
