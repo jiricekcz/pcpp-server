2e9e96e17b3759b691310368854accf34163f167
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2021-04-25 13:44:38 +0200

    removed compilations

--- compilation/setup.py
+++ compilation/setup.py
@@ -1,1 +1,14 @@
-print("addaa")
+import setuptools
+
+h = setuptools.Extension(
+        "h",
+        sources=["./h_module.cpp"]
+)
+setuptools.setup(
+    name="h",
+    version="1.0",
+    author="jiricekcz",
+    license="MIT",
+    ext_modules=[h],
+    packages=["h"]
+)
